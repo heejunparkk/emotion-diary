22f0010c119b0e431829da26d72f1782ab6f5752
heejun <gmlwns2002@gmail.com>
2022-02-22 21:05:51 +0900

    diary list array

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -21,8 +21,19 @@ const DiaryList = ({ diaryList }) => {
   const [sortType, setSortType] = useState("lastest");
 
   const getProcessedDiaryList = () => {
+    const compare = (a, b) => {
+      if (sortType === "lastest") {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    }; //if문으로 정렬된 리스트를 반환해주는 함수
+
     const copyList = JSON.parse(JSON.stringify(diaryList));
-  }; //diaryList가 배열인데 Json화 시켜서 문자로 바꿔주고 parse를 실행시키게되면 다시 배열로 복구된다. 그럼 { diaryList }배열을 건드리지 않을수있다.
+    //깊은복사 해준다. diaryList가 배열인데 Json화 시켜서 문자로 바꿔주고 parse를 실행시키게되면 다시 배열로 복구된다. 그럼 { diaryList }배열을 건드리지 않을수있다.
+    const sortedList = copyList.sort(compare);
+    return sortedList;
+  };
 
   return (
     <div>
@@ -31,7 +42,7 @@ const DiaryList = ({ diaryList }) => {
         onChange={setSortType}
         optionList={sortOptionList}
       />
-      {diaryList.map((it) => (
+      {getProcessedDiaryList().map((it) => (
         <div key={it.id}>{it.content}</div>
       ))}
     </div>
