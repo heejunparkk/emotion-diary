fe967aa4b0875a90810243b7ddef7c2e15f21cf9
heejun <gmlwns2002@gmail.com>
2022-02-23 20:20:01 +0900

    diary header

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import MyButton from "./MyButton";
 
 const sortOptionList = [
   { value: "lastest", name: "최신순" },
@@ -13,7 +15,11 @@ const filterOptionList = [
 
 const ControlMenu = ({ value, onChange, optionList }) => {
   return (
-    <select value={value} onChange={(e) => onChange(e.target.value)}>
+    <select
+      className="ControlMenu"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    >
       {optionList.map((it, idx) => (
         <option key={idx} value={it.value}>
           {it.name}
@@ -24,10 +30,19 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 };
 
 const DiaryList = ({ diaryList }) => {
+  const navigate = useNavigate();
   const [sortType, setSortType] = useState("lastest");
   const [filter, setFilter] = useState("all");
 
   const getProcessedDiaryList = () => {
+    const filterCallback = (item) => {
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
+      }
+    };
+
     const compare = (a, b) => {
       if (sortType === "lastest") {
         return parseInt(b.date) - parseInt(a.date);
@@ -36,24 +51,38 @@ const DiaryList = ({ diaryList }) => {
       }
     }; //if문으로 정렬된 리스트를 반환해주는 함수
 
-    const copyList = JSON.parse(JSON.stringify(diaryList));
-    //깊은복사 해준다. diaryList가 배열인데 Json화 시켜서 문자로 바꿔주고 parse를 실행시키게되면 다시 배열로 복구된다. 그럼 { diaryList }배열을 건드리지 않을수있다.
-    const sortedList = copyList.sort(compare);
+    const copyList = JSON.parse(JSON.stringify(diaryList)); //깊은복사 해준다. diaryList가 배열인데 Json화 시켜서 문자로 바꿔주고 parse를 실행시키게되면 다시 배열로 복구된다. 그럼 { diaryList }배열을 건드리지 않을수있다.
+
+    const filteredList =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallback(it));
+    const sortedList = filteredList.sort(compare);
     return sortedList;
   };
 
   return (
-    <div>
-      <ControlMenu
-        value={sortType}
-        onChange={setSortType}
-        optionList={sortOptionList}
-      />
-      <ControlMenu
-        value={filter}
-        onChange={setFilter}
-        optionList={filterOptionList}
-      />
+    <div className="DiaryList">
+      <div className="menu_wrapper">
+        <div className="left_col">
+          <ControlMenu
+            value={sortType}
+            onChange={setSortType}
+            optionList={sortOptionList}
+          />
+          <ControlMenu
+            value={filter}
+            onChange={setFilter}
+            optionList={filterOptionList}
+          />
+        </div>
+        <div className="right_col">
+          <MyButton
+            type={"positive"}
+            text={"새 일기쓰기"}
+            onClick={() => navigate("/new")}
+          />
+        </div>
+      </div>
+
       {getProcessedDiaryList().map((it) => (
         <div key={it.id}>
           {it.content} {it.emotion}
